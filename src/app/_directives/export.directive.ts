fb641f72839220e31721b52b02379d58eb7417b7
ranjan.gaya786@gmail.com <rkr7277593672>
2020-02-05 22:32:25 +0530

    change

---
+++ src/app/_directives/export.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appExport]'
+})
+export class ExportDirective {
+
+  constructor() { }
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import {ExportService} from './_services/export.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pro';
+  
+
+  customers: any = [];
+  constructor(private exportService: ExportService) {}
+
+  ngOnInit() {
+    for (let i = 0; i <= 25; i++) {
+      this.customers.push({firstName: `first${i}`, lastName: `last${i}`,
+      email: `abc${i}@gmail.com`, address: `000${i} street city, ST`, zipcode: `0000${i}`});
+    }
+  }
+
+  export() {
+    this.exportService.exportExcel(this.customers, 'customers');
+  }
+
+}
